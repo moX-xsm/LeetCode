87c1e28e5ca270ef6be6d5a6bffe8490159c424d
moX-xsm <xsm970228@163.com>
2020-02-26 12:11:35 +0800

    add Leetcode 19 24 83 141 142 160

---
+++ 83.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* deleteDuplicates(struct ListNode* head){
+    struct ListNode *p, *q;
+    p = head;
+    while(p && p->next){
+        if(p->val - p->next->val){
+            p = p->next;
+        }else{
+            q = p->next;
+            p->next = q->next;
+            free(q);
+        }
+    }
+    return head;
+}
