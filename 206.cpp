ce86f97496a4edd6843beed14df63e6a7644e4c4
moX-xsm <xsm970228@163.com>
2020-02-27 15:38:54 +0800

    add LeetCode 202 203 206

---
+++ 206.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+    struct ListNode ret, *p, *q;
+    ret.next = NULL;
+    if(head == NULL) return head;
+    p = head;
+    while(p){
+        q = p->next;
+        p->next = ret.next;
+        ret.next = p;
+        p = q; 
+    }
+    return ret.next;
+}
